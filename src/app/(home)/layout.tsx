5c80e94d64d757dfa0615c757e4119562e16d806
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-01-08 15:38:32 +0700

    feat: change static to dynamic data in navbar

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,5 +1,3 @@
-import { NavBar } from "@/components/navbar";
-
 export default function DashboardLayout({
   children,
 }: {
@@ -7,7 +5,6 @@ export default function DashboardLayout({
 }) {
   return (
     <main className="antialiased max-w-3xl space-y-16 border-gray-700 md:border-2 md:rounded-xl py-5 px-7 mx-auto md:my-[20vh] min-h-screen">
-      <NavBar />
       {children}
     </main>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,12 +3,18 @@
 import * as React from "react";
 import { DollarSign } from "lucide-react";
 
-export const NavBar = () => {
+interface NavBarProps {
+  isAvailable: boolean;
+}
+
+export const NavBar: React.FC<NavBarProps> = ({ isAvailable }) => {
   return (
     <nav className="flex justify-end items-center">
-      <span className="inline-flex items-center font-medium rounded-md text-xs px-2 py-1 bg-primary/25 dark:bg-primary/25 dark:bg-opacity-10 text-primary dark:text-primary-400 ring-1 ring-inset ring-primary dark:ring-primary ring-opacity-25 dark:ring-opacity-25">
-        Available for Work <DollarSign className="ml-1" size={16} />
-      </span>
+      {isAvailable ? (
+        <span className="inline-flex items-center font-medium rounded-md text-xs px-2 py-1 bg-primary/25 dark:bg-primary/25 dark:bg-opacity-10 text-primary dark:text-primary-400 ring-1 ring-inset ring-primary dark:ring-primary ring-opacity-25 dark:ring-opacity-25">
+          Available for Work <DollarSign className="ml-1" size={16} />
+        </span>
+      ) : null}
     </nav>
   );
 };
