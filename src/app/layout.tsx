5cf886eb763309a0e60af535d91d6ad106146612
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-01-09 18:38:57 +0700

    feat: add scroll and test meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import ScrollProgress from "@/components/ui/scroll-progress";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,6 +21,21 @@ export const metadata: Metadata = {
   title: "Lorn Samnang",
   description:
     "I am a Software Developer, highly perceptive and very imaginative by seeking inspiration in every moment.",
+  openGraph: {
+    type: "website",
+    url: "https://res.cloudinary.com/dnye8vqwb/image/upload/v1734708545/images/7824885_lj2gkw.jpg",
+    title: "Lorn Samnang",
+    description:
+      "I am a Software Developer, highly perceptive and very imaginative by seeking inspiration in every moment.",
+    images: [
+      {
+        url: "/favicon.ico",
+        width: 1200,
+        height: 630,
+        alt: "Og Image Alt",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -30,6 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning={true}>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <ScrollProgress />
         {children}
         <Toaster richColors theme="light" position="top-center" />
         <Analytics />
