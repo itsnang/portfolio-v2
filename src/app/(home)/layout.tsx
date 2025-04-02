fcf0339e6dcff21005b17251d04190a261bdcb34
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-04-02 17:33:01 +0700

    feat: change layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,7 +4,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="antialiased max-w-3xl space-y-16 border-gray-700 md:border-2 md:rounded-xl py-5 px-7 mx-auto md:my-[20vh] min-h-screen">
+    <main className="antialiased max-w-3xl space-y-12 border-gray-700 md:border-2 md:rounded-xl py-5 px-7 mx-auto md:my-[20vh] min-h-screen">
       {children}
     </main>
   );
