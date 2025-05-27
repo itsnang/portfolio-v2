461a7ddd75c757e30341704fd9509aca2cb4bfb4
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-05-27 13:57:31 +0700

    fix: adjust layout

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,7 @@ interface NavBarProps {
 
 export const NavBar: React.FC<NavBarProps> = ({ isAvailable }) => {
   return (
-    <nav className="flex justify-end items-center">
+    <nav className="flex justify-end items-center mb-6">
       {isAvailable ? (
         <span className="inline-flex items-center font-medium rounded-md text-xs px-2 py-1 bg-primary/25 dark:bg-primary/25 dark:bg-opacity-10 text-primary dark:text-primary-400 ring-1 ring-inset ring-primary dark:ring-primary ring-opacity-25 dark:ring-opacity-25">
           Available for Work <DollarSign className="ml-1" size={16} />
