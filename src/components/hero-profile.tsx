b43592e38280f01872ccbba60947a2541664051d
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-07-11 22:17:44 +0700

    feat: add SVG loader configuration and adjust hero profile height for responsive design

--- src/components/hero-profile.tsx
+++ src/components/hero-profile.tsx
@@ -15,7 +15,7 @@ interface HeroProfileProps {
 export function HeroProfile({ profile, className = "" }: HeroProfileProps) {
   return (
     <div
-      className={`w-full bg-linear-to-r from-blue-200 to-cyan-200 lg:h-80 relative mb-[114px] h-40 block rounded-lg ${className}`}
+      className={`w-full bg-linear-to-r from-blue-200 to-cyan-200 lg:h-80 relative mb-[114px] md:h-60 h-40 block rounded-lg ${className}`}
     >
       <Image
         src="/cover.jpg"
