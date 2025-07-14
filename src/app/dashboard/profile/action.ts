23ee8e1623f36562d086956dc32957734717c173
itsnang <lorn.samnang.1221@rupp.edu.kh>
2025-07-14 23:57:26 +0700

    feat: update profile action to fetch current user and use user ID for profile updates

--- src/app/dashboard/profile/action.ts
+++ src/app/dashboard/profile/action.ts
@@ -2,16 +2,22 @@
 import { db } from "@/db/drizzle";
 import { ProfileInsert } from "@/db/schema/profile.schema";
 import { TbProfile } from "@/db/table";
+import { getCurrentUser } from "@/lib/auth/get-current-user";
 import { InsertError } from "@/lib/errors";
 import { err, ok } from "@justmiracle/result";
 import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 
 export const updateProfileAction = async (profie: ProfileInsert) => {
+  const user = await getCurrentUser();
+  if (!user) {
+    throw new Error("User not found");
+  }
+  const userId = user?.id;
   const insertProfile = await db
     .update(TbProfile)
     .set(profie)
-    .where(eq(TbProfile.id, "1"))
+    .where(eq(TbProfile.id, userId))
     .returning()
     .then(ok)
     .catch(err);
